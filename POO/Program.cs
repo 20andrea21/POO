b80a812e090a40205090307613fb4e67ee94f45b
20andrea21 <asbsap@gmail.com>
2021-01-26 17:06:30 -0300

    MI VERSION

---
+++ POO/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace POO
+{
+    public class Persona
+    {
+      public string Nombre;
+      public string Apellido;
+      public string DNI;
+      public DateTime FechaNacimiento;
+      public Persona()
+      {
+
+      }
+      public Persona(string nombre, string apellido, string dni, DateTime fechaNac)
+      {
+          Nombre= nombre;
+          Apellido= apellido;
+          DNI= dni;
+          FechaNacimiento= fechaNac; 
+      }
+    public class Profesor: Persona
+    {
+        public string Materia;
+    }
+
+    public class Alumno: Persona
+    {
+        public string Legajo;
+    }
+
+    public class Curso
+    {
+        public Alumno[] Alumnos;
+        public Profesor[] Profesores;
+    }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Persona persona1= new Persona() ;
+        }
+    }
+}
---
+++ app/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N1=7;
+            int N2=6;
+            int Suma=N1+N2;
+            int Resta=N1-N2;
+            int Multiplicacion=N1*N2;
+            double Division=N1/N2;
+
+            string[] MiListaNombre= new string[5] {"Eugenio","Emma","Lorenzo","Ricardo","Veronica"};
+
+            Console.WriteLine("La Suma es igual a: "+Suma);
+            Console.WriteLine("La Resta es igual a: "+Resta);
+            Console.WriteLine("La Multiplicación es igual a: "+Multiplicacion);
+            Console.WriteLine("La División es igual a: "+Division);
+
+            for (int i=0; i<5; i++)
+            {
+                Console.WriteLine(MiListaNombre[i]);
+            }
+
+            for (int i=0; i<5; i++)
+            {
+                if (MiListaNombre[i]=="Emma")
+                {
+                   Console.WriteLine(MiListaNombre[i]+" sos la ganadora!");   
+                }
+                else
+                {
+                Console.WriteLine(MiListaNombre[i]);
+                }
+            }
+        }
+    }
+}
